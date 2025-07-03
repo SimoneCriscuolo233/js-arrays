2f82b46f657d1619ba22d485fab49e221281944d
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-07-03 14:38:26 +0200

    [UPD] Bonus 5: Implement teachersString to join teachers array into a comma-separated string and log the result

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -36,4 +36,5 @@ if (isFabioPresent !== -1) {
 
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.join(", ");
+console.log(teachersString);
